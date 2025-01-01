b4af987a874f63b7acb379bc23125fd7d33ff04b
algo-boy <vhienanuta.cs@gmail.com>
2025-01-02 07:09:38 +0800

    Add setup settings functionality
    - Implemented control key assignment for both players with uniqueness validation
    - Ensured rope length is an odd number within the range that fits on the screen

--- tug-of-war.c
+++ tug-of-war.c
@@ -1,43 +1,144 @@
 #include <stdio.h>
 #include <conio.h>
 
-#define SCREEN_X 80
-#define SCREEN_Y 25
-#define SCREEN_CENTER_X SCREEN_X / 2
-#define SCREEN_CENTER_Y SCREEN_Y / 2 // Integer division
+#define SCREEN_WIDTH 80
+#define SCREEN_HEIGHT 25
+#define SCREEN_CENTER_X SCREEN_WIDTH / 2
+#define SCREEN_CENTER_Y SCREEN_HEIGHT / 2 // Integer division
 
 #define ROPE_LENGTH 11 // Best to be an odd integer
 
-#define HIDE_CURSOR gotoxy(SCREEN_X, SCREEN_Y)
+#define HIDE_CURSOR gotoxy(SCREEN_WIDTH, SCREEN_HEIGHT)
+
+void display_title_screen() {
+    clrscr();
+    
+    gotoxy(1, 11);
+    printf("                   _                   ___\n");
+    printf(" _ ___ ___ ___ ___| |_ _ _ ___ ___ ___|  _|___ _ _ _ ___ ___ ___ ___ ___ ___ __\n");
+    printf(" _|___|___|___|___|  _| | | . |___| . |  _|___| | | | .'|  _|___|___|___|___|__\n");
+    printf("                  |_| |___|_  |   |___|_|     |_____|__,|_|\n");
+    printf("                          |___|\n");
+    
+    gotoxy(27, 17);
+    printf("[ PRESS ANY KEY TO START ]");
+    
+    HIDE_CURSOR;
+    
+    getch();
+}
+
+void get_settings(char *player1_key, char *player2_key, int *rope_length) {
+    clrscr();
+    
+    gotoxy(25, 11);
+    printf("___________SETTINGS___________");
+    
+    gotoxy(25, 15);
+    printf("______________________________");
+    
+    gotoxy(25, 12);
+    printf("Enter key for Player 1: ");
+    *player1_key = toupper(getchar());
+    
+    while (getchar() != '\n'); // Clears input buffer
+    
+    // Gets Player 2's control key and compares it to Player 1's
+    while (1) {
+        int i;
+        
+        gotoxy(25, 13);
+        
+        for (i = 1; i <= SCREEN_WIDTH - 1; i++) {
+            printf(" ");
+        }
+        
+        gotoxy(25, 13);
+        printf("Enter key for Player 2: ");
+        *player2_key = toupper(getchar());
+        
+        while (getchar() != '\n');
+        
+        if (*player2_key == *player1_key) {
+            gotoxy(15, 16);
+            printf("Player 2 key must not be the same as Player 1 key.");
+            
+            gotoxy(27, 17);
+            printf("Press any key to continue.");
+            
+            getch();
+            
+            // Clears warning message
+            gotoxy(15, 16);
+            printf("                                                  ");
+            
+            gotoxy(27, 17);
+            printf("                          ");
+        } else break;
+    }
+    
+    // Gets and validates rope length
+    while (1) {
+        int i;
+        
+        gotoxy(1, 14);
+        
+        for (i = 1; i <= SCREEN_WIDTH - 1; i++) {
+            printf(" ");
+        }
+        
+        gotoxy(25, 14);
+        printf("Enter length of rope: ");
+        
+        if (scanf("%d", rope_length) != 1 || *rope_length <= 5 || *rope_length >= 39 || *rope_length % 2 == 0) {
+            gotoxy(14, 16);
+            printf("Rope length must be an odd integer between 5 and 39.");
+            
+            while (getchar() != '\n');
+            
+            gotoxy(27, 17);
+            printf("Press any key to continue.");
+            
+            getch();
+            
+            // Clears warning message
+            gotoxy(14, 16);
+            printf("                                                    ");
+            
+            gotoxy(27, 17);
+            printf("                          ");
+        } else break;
+    }
+}
 
 void draw_center_line() {
     int y;
     
     clrscr();
     
-    for (y = 1; y <= SCREEN_Y; y++) {
+    for (y = 1; y <= SCREEN_HEIGHT; y++) {
         gotoxy(SCREEN_CENTER_X, y);
         printf("|");
     }
 }
 
-void update_rope(int rope_start) {
+void update_rope(int rope_start, int rope_length) {
     int i;
     
     // Clears the previous rope
     gotoxy(1, SCREEN_CENTER_Y);
     
-    for (i = 1; i <= SCREEN_X; i++) {
+    for (i = 1; i <= SCREEN_WIDTH; i++) {
         printf(" ");
     }
     
     // Draws the new rope
     gotoxy(rope_start, SCREEN_CENTER_Y);
     
-    for (i = 1; i <= ROPE_LENGTH; i++) {
+    for (i = 1; i <= rope_length; i++) {
         if (i == 1) {
             printf("o");
-        } else if (i == ROPE_LENGTH) {
+        } else if (i == rope_length) {
             printf("o");
         } else {
             printf("-");
@@ -48,15 +149,23 @@ void update_rope(int rope_start) {
 }
 
 int main() {
-    int rope_start = SCREEN_CENTER_X - (ROPE_LENGTH / 2);
-    char input;
+    int rope_length, rope_start;
+    char player1_key, player2_key;
+    
+    display_title_screen();
+    
+    get_settings(&player1_key, &player2_key, &rope_length);
     
     draw_center_line();
     
+    rope_start = SCREEN_CENTER_X - (rope_length / 2);
+    
     while (1) {
-        update_rope(rope_start);
+        char input;
         
-        if (rope_start == SCREEN_CENTER_X - ROPE_LENGTH + 1) {
+        update_rope(rope_start, rope_length);
+        
+        if (rope_start == SCREEN_CENTER_X - rope_length + 1) {
             gotoxy(33, SCREEN_CENTER_Y - 1);
             printf("Player 1 wins...");
             
@@ -70,9 +179,9 @@ int main() {
         
         input = toupper(getch());
         
-        if (input == 'F') {
+        if (input == player1_key) {
             rope_start--;
-        } else if (input == 'J') {
+        } else if (input == player2_key) {
             rope_start++;
         } else if (input == '0') {
             break;
