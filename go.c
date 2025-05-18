55b6d7f2bb05ce18792ff9df97973d58970ead60
algo-boy <vhienanuta.cs@gmail.com>
2025-05-18 08:00:12 +0800

    Change board array size to 11 by 11 to account for off-the-board spaces

--- go.c
+++ go.c
@@ -16,21 +16,35 @@ void hide_cursor() {
     SetConsoleCursorInfo(hConsole, &cursorInfo);
 }
 
-char board[9][9];
-char stone = '0'; // 0 = black, O = white
+typedef struct Node {
+	int x, y;
+	struct Node *next;
+} Node;
+
+int board[11][11]; // +2 for off-board spaces
+char states[5] = {'+', '0', 'O', '+', 'X'}; // [0] = empty, [1] = black, [2] = white, [3] = liberty, [4] = off-board
+
+int game_running = 1;
+int turn = 1; // 1 = black, 2 = white
 
 void update_board();
 void initialize_board();
 void place_stone();
+void detect_group();
 
 int main() {
 	hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 	
 	initialize_board();
-	update_board();
-	place_stone();
+	
+	while (game_running) {
+		update_board();
+		place_stone();
+	}
 	
 	getch();
+	
+	return 0;
 }
 
 void place_stone() {
@@ -40,31 +54,30 @@ void place_stone() {
 	printf("| Enter the position of stone to place (x y): ");
 	scanf("%d %d", &x, &y);
 	
-	--x;
-	--y;
-	
-	if (x < 0 || x > 9 || y < 0 || y > 9 || board[x][y] != '+') {
+	if (x < 0 || x > 9 || y < 0 || y > 9 || board[x][y] != 0) {
 	   	gotoxy(41, 5);
 	   	printf("| Invalid move. Press any key to try again.");
 	   	
 	   	getch();
 	   	
-	   	update_board();
+	   	return;
 	}
 	
-	board[x][y] = stone;
-    
-    stone = (stone == '0') ? 'O' : '0';
-    
-	update_board();
+	board[x][y] = turn;
+        
+    turn = (turn == 1) ? 2 : 1;
 }
 
 void initialize_board() {
     int x, y;
     
-    for (x = 0; x < 9; x++) {
-        for (y = 0; y < 9; y++) {
-            board[x][y] = '+';
+    for (x = 0; x < 11; x++) {
+        for (y = 0; y < 11; y++) {
+			if (x == 0 || x == 10 || y == 0 || y == 10) {
+				board[x][y] = 4;
+			} else {
+				board[x][y] = 0;
+			}
         }
     }
 }
@@ -74,18 +87,18 @@ void update_board() {
     
     system("cls");
     
-    for (y = 0; y < 9; y++) {
+    for (y = 1; y < 10; y++) {
 		int square_start_x = 4;
 		
-    	for (x = 0; x < 9; x++) {
+    	for (x = 1; x < 10; x++) {
     		gotoxy(square_start_x, square_start_y);
-			printf("%c", board[x][y]);
+			printf("%c", states[board[x][y]]);
 			
-			if (x != 8) {
+			if (x != 9) {
 				printf("---");
 			}
 			
-			if (y != 8) {
+			if (y != 9) {
 				gotoxy(square_start_x, square_start_y + 1);
 				printf("|");
 			}
@@ -95,6 +108,4 @@ void update_board() {
 		
 		square_start_y += 2;
 	}
-    
-    place_stone();
 }
